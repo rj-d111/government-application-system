854297fe60969922ffacf323283eb5c603d78e52
rj-d111 <raymondaguillar1@gmail.com>
2023-12-14 06:00:11 +0800

    btn-teal

---
+++ index.php
@@ -0,0 +1,58 @@
+<!-- Connect navbar to the page -->
+<?php include "navbar.php" ?>
+
+    <!-- Start of main content -->
+    
+    <!-- Background Picture First -->
+    <section class="container">
+        <div class="col-md-6 text-teal">
+            <h1 class="pt-5">Why Register?</h1>
+            <p>In the current digital era, where security and convenience go hand in hand, the Digital Government ID Application System (DGID) shows promise as a revolutionary tool. Accessing public and private institutions is made simple with your DGID, doing away with the need for laborious paperwork and repeated trips. The benefits extend beyond simplicity of use. The DGID system employs contemporary security measures to thwart identity theft and unauthorized access. This robust system ensures that only you have power over your information, giving you the confidence to successfully validate your identity online.</p>    
+        </div>
+        <a href="customer/fill-out.php" class="btn btn-teal rounded-pill text-white btn-lg">Start Now</a>
+    </section>
+    <!-- End of Background Picture -->
+    
+    
+    
+    <!-- End of main content -->
+
+    <!-- Start of Footer -->
+    <footer class="bg-secondary text-white text-center text-lg-start mt-5">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!--Grid row-->
+            <div class="row">
+                <!--Grid column-->
+                <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">ABOUT</h5>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                        molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                        voluptatem veniam, est atque cumque eum delectus sint!
+                    </p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">CONTACT US</h5>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                        molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                        voluptatem veniam, est atque cumque eum delectus sint!
+                    </p>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+
+    </footer>
+    <!-- End of Footer -->
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Fill out Form</title>
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+</head>
+
+<body>
+    <!-- Start of Logo -->
+    <div class="container d-flex align-items-center my-3 mx-auto text-center">
+        <div class="d-flex align-items-center me-auto">
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <img src="img/government-registration-logo.png" alt="Bootstrap" style="max-width: 50px; margin-right: 10px;">
+                <h2 class="text-teal ms-0">ONLINE GOVERNMENT APPLICATION SYSTEM</h2>
+            </a>
+        </div>
+        <div class="text-end">
+            <a href="admin/login.php" class="text-teal text-decoration-none fw-bold">ADMIN LOGIN</a>
+        </div>
+    </div>
+
+    <!-- End of Logo -->
+    <!-- Start of Navbar -->
+    <nav class="nav justify-content-center bg-teal">
+        <a class="nav-link  link-light active" aria-current="page" href="#">HOME</a>
+        <a class="nav-link  link-light" href="#">REGISTER</a>
+        <a class="nav-link  link-light" href="#">HOW TO APPLY</a>
+        <a class="nav-link  link-light" href="#">REQUIREMENTS</a>
+        <a class="nav-link  link-light" href="#">CHECK STATUS</a>
+        <a class="nav-link  link-light" href="#">CONTACT US</a>
+    </nav>
+    <!-- End of Navbar -->
